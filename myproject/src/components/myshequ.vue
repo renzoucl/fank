a759327fbe1a010ac2379ca6892e3c8fd32352c1
qqempire <1508308580@qq.com>
2018-08-28 21:05:28 +0800

    第六次提交，icon图标更新

--- myproject/src/components/myshequ.vue
+++ myproject/src/components/myshequ.vue
@@ -4,11 +4,14 @@
                 <div class="left">
                         <span class="iconfont icon-arrow_left_f"></span>
                 </div>
-                <div class="middle"></div>
+                <div class="middle">我的社区</div>
                 <div class="right">
                         <span class="iconfont icon-gengduo"></span>
                 </div>
             </header>
+            <div id="bj">
+               
+            </div>
         </div>
  </template>
       
@@ -26,11 +29,12 @@
       
 <style scoped>
       header{height: 45px;width: 100%;background: #222222;display: flex;justify-content: space-between}
-      header .left{width: 20%;height: 45px;background: red}
-      header .middle{width: 60%;height: 45px;background: greenyellow}
-      header .right{width: 20%;height: 45px;background: red}
-      header .left span{font-size: 16px}
+      header .left{width: 10%;height: 45px;text-align: center;line-height: 45px}
+      header .middle{width: 60%;height: 45px;text-align: center;line-height: 45px;font-weight: bold;color: #fff}
+      header .right{width: 10%;height: 45px;text-align: center;line-height: 45px}
+      header .left span{font-size: 26px;color: #fce452}
+      header .right span{font-size: 20px;color: #fce452}
 
-
+     #bj{width: 100%;height: 80px;background: red}
 </style>
       
